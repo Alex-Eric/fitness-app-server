cd0f0f21fb5bd445f5feaaf37eacdbcfa323f18f
ericveganoguera <eveno229@gmail.com>
2023-04-14 00:14:26 +0200

    Add route to list exercise

--- routes/exercise.routes.js
+++ routes/exercise.routes.js
@@ -1,28 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const request = require("request");
+const Exercise = require("../models/Exercise.model");
 
 // GET /api/exercises
 router.get("/exercises", (req, res, next) => {
-  request.get(
-    {
-      url: "https://api.api-ninjas.com/v1/exercises?muscle=biceps",
-      headers: {
-        "X-Api-Key": "SESp/8Nz4NWIYG6FDDerNA==RD1ZbMZltIomlxMU",
-      },
-      json:true
-    },
-    function (error, response, body) {
-      if (error) return console.error("Request failed:", error);
-      else if (response.statusCode != 200)
-        return console.error(
-          "Error:",
-          response.statusCode,
-          body.toString("utf8")
-        );
-      else res.json(body)
-    }
-  );
+  Exercise.find()
+  .then(response=>res.json(response))
+  .catch(err=>console.log("Error: " , err))
 });
 
 module.exports = router;
