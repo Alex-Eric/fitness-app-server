8ea4db10816658c964922297397834700485fb51
Alex Uceda <alexucedacintas@gmail.com>
2023-04-13 17:43:17 +0200

    add routes muscles

--- routes/muscle.routes.js
+++ routes/muscle.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const Muscle = require("../models/Muscle.model");
 const router = express.Router();
 
 // GET /api/muscles
 router.get("/muscles",(req,res,next)=>{
-    res.json("Muscles!")
+    Muscle.find()
+    .then((response)=>{
+        res.json(response)
+    })
 })
 
 module.exports = router;
